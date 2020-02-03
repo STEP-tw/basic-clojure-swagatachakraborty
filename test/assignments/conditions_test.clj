4ae4a73c52b6b3c2ad8109de4eaf0e884b831234
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2020-02-03 11:09:43 +0530

    Add implementation of Harischandra

--- test/assignments/conditions_test.clj
+++ test/assignments/conditions_test.clj
@@ -13,3 +13,15 @@
     (is (= 2 (informative-divide 4 2))))
   (testing "zero denominator returning :infinite"
     (is (= :infinite (informative-divide 3 0)))))
+
+(deftest raja-harishchandra
+  (testing "should return true for true"
+    (is (true? (harishchandra true))))
+  (testing "should return true for 2"
+    (is (= 2 (harishchandra 2))))
+  (testing "should return true for []"
+    (is (= [] (harishchandra []))))
+  (testing "should return false for false"
+    (is (= nil (harishchandra false))))
+  (testing "should return false for nil"
+    (is (= nil (harishchandra nil)))))
